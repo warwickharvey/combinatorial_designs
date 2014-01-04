0afa8e683cc69ab66ddac26894f8189ef4087c42
Warwick Harvey <warwick@triposo.com>
2014-01-04 20:18:07 +0100

    Add clearing of constructions from the database

--- golf/constructions.py
+++ golf/constructions.py
@@ -10,19 +10,44 @@ class Constructor(object):
     """
     Base class for constructors
     """
-    def __init__(self):
-        user, _ = models.User.objects.get_or_create(
-            name=self.name,
-            email=self.email,
-        )
-        citation, _ = models.Citation.objects.get_or_create(citation=self.description)
-        construction_info, _ = models.ConstructionInfo.objects.get_or_create(id=self.id, version=self.version)
-        self.submission_info, _ = models.SubmissionInfo.objects.get_or_create(citation=citation, submitter=user, construction=construction_info)
+    _submission_info = None
 
-    # TODO: Add a method for deleting (old) solutions/bounds created by this
-    # constructor
+    def submission_info(self):
+        """
+        Returns a SubmissionInfo instance for use by the constructor.
+        Assumes the subclass has defined the following fields for the
+        constructor:
+            id: A unique string ID
+            version: An integer version number
+            name: A human-readable name
+            email: An email address for the author/maintainer
+            description: A description
+        """
+        if not self._submission_info:
+            user, _ = models.User.objects.get_or_create(
+                name=self.name,
+                email=self.email,
+            )
+            citation, _ = models.Citation.objects.get_or_create(citation=self.description)
+            construction_info, _ = models.ConstructionInfo.objects.get_or_create(id=self.id, version=self.version)
+            self._submission_info, _ = models.SubmissionInfo.objects.get_or_create(citation=citation, submitter=user, construction=construction_info)
+        return self._submission_info
+
+    def clear_constructions(self):
+        """
+        Deletes all constructions by this constructor from the database
+        """
+        self._submission_info = None
+        models.ConstructionInfo.objects.filter(id=self.id).delete()
 
     def construct(self, instance):
+        """
+        Performs the construction for the given instance.  Returns the
+        constructed item, or None if the construction is not applicable for
+        this instance.
+        Calls do_construct() to do the work, and then calls save() on the
+        result, if any.
+        """
         bound = self.do_construct(instance)
         if bound:
             bound.save()
@@ -43,7 +68,7 @@ class TrivialSolutionConstructor(Constructor):
     def do_construct(self, instance):
         # TODO: Actually construct a solution rather than providing just the
         # bound
-        return models.GolfLowerBound(instance=instance, submission_info=self.submission_info, num_rounds=2)
+        return models.GolfLowerBound(instance=instance, submission_info=self.submission_info(), num_rounds=2)
 
 
 class TrivialUpperBoundConstructor(Constructor):
@@ -59,21 +84,23 @@ class TrivialUpperBoundConstructor(Constructor):
 
     def do_construct(self, instance):
         bound = (instance.num_players() - 1) / (instance.group_size - 1)
-        return models.GolfUpperBound(instance=instance, submission_info=self.submission_info, num_rounds=bound)
+        return models.GolfUpperBound(instance=instance, submission_info=self.submission_info(), num_rounds=bound)
 
 
 class Constructors(object):
     """
     Class for managing and running Constructor instances
     """
-    constructors = None
+    _constructors = None
     _instances = None
 
-    def __init__(self):
-        self.constructors = [
-            TrivialSolutionConstructor(),
-            TrivialUpperBoundConstructor(),
-        ]
+    def constructors(self):
+        if not self._constructors:
+            self._constructors = [
+                TrivialSolutionConstructor(),
+                TrivialUpperBoundConstructor(),
+            ]
+        return self._constructors
 
     def instances(self):
         """
@@ -94,7 +121,8 @@ class Constructors(object):
         """
         Run all constructors on all instances
         """
-        for constructor in self.constructors:
+        for constructor in self.constructors():
+            constructor.clear_constructions()
             for instance in self.instances():
                 constructor.construct(instance)
 
